8af33c9c73119e55475ea83ff83225ddaa691b3c
Pirikita <beatriz.oliveira.work@gmail.com>
2025-11-12 17:25:25 +0100

    wrote the code to both exercises node test

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,5 +1,7 @@
 // Predict and explain first...
 
+ // I think it will show a syntax error due to inside const address we have more data than just the house number as requested.
+ 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
