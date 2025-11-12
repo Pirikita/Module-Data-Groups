c3ee6b9f623c032501a3c074bba86462b02cd23e
Pirikita <beatriz.oliveira.work@gmail.com>
2025-11-12 17:49:47 +0100

    corrected the code in order to work

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,17 +1,18 @@
 // Predict and explain first...
 
  // I think it will show a syntax error due to inside const address we have more data than just the house number as requested.
+ // My house number is undefined - not a syntax 
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
 
-const address = {
-  houseNumber: 42,
-  street: "Imaginary Road",
-  city: "Manchester",
-  country: "England",
-  postcode: "XYZ 123",
-};
+const address = [
+  houseNumber = 42,
+  street = "Imaginary Road",
+  city = "Manchester",
+  country = "England",
+  postcode = "XYZ 123",
+];
 
 console.log(`My house number is ${address[0]}`);
