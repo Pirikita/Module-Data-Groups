2e54027fd964641cadb1fb2117c4862a63d036fe
Pirikita <beatriz.oliveira.work@gmail.com>
2025-11-13 11:54:15 +0100

    - implemented function that checks if an object contains a particular property - node test

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,34 @@
-function contains() {}
+function contains(obj, prop) { // define parameters
+    if (typeof obj !== 'object' || obj === null || Array.isArray(obj)) {
+        return false;
+    }
+        return Object.prototype.hasOwnProperty.call(obj, prop);
+}   
+
+
+// Given a contains function
+// When passed an object and a property name
+// Then it should return true if the object contains the property, false otherwise
+console.log(contains({a: 1, b: 2}, 'a')); // true
+
+// Given an empty object
+// When passed to contains
+// Then it should return false
+console.log(contains({}, 'a')); // false
+
+// Given an object with properties
+// When passed to contains with an existing property name
+// Then it should return true
+console.log(contains({a: 1, b: 2}, 'a')); // true
+
+// Given an object with properties
+// When passed to contains with a non-existent property name
+// Then it should return false
+console.log(contains({a: 1, b: 2}, 'c')); // false
+
+// Given invalid parameters like an array
+// When passed to contains
+// Then it should return false or throw an error
+console.log(contains([], 'a')); // false
 
 module.exports = contains;
--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -20,7 +20,7 @@ as the object doesn't contains a key of 'c'
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+//test.todo("contains on empty object returns false");
 
 // Given an object with properties
 // When passed to contains with an existing property name
