45cff676f76021a91d03282eb5f11b506417202b
Pirikita <beatriz.oliveira.work@gmail.com>
2025-11-13 13:17:26 +0100

    installed json to run tests

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -16,6 +16,9 @@ as the object doesn't contains a key of 'c'
 // Given a contains function
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
+test("object contains property", () => {
+    expect(contains({a: 1, b: 2}, 'a')).toEqual(true);
+})
 
 // Given an empty object
 // When passed to contains
