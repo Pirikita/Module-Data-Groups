7af131622daf3d21afdfc254c34b71e4f224eac7
Pirikita <beatriz.oliveira.work@gmail.com>
2025-11-13 13:25:28 +0100

    wrote test cases for all accepted criteria tested and corrected issues

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -23,16 +23,27 @@ test("object contains property", () => {
 // Given an empty object
 // When passed to contains
 // Then it should return false
-//test.todo("contains on empty object returns false");
+test("empty object", () => {
+    expect(contains({}, 'a')).toEqual(false);
+})
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
+test("object contains existing property", () => {
+    expect(contains({a: 1, b: 2}, 'a')).toEqual(true);
+})
 
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
+test("object does not contain property", () => {
+    expect(contains({a: 1, b: 2}, 'c')).toEqual(false);
+})
 
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
+test("invalid parameters", () => {
+    expect(contains([], 'a')).toEqual(false);
+})
