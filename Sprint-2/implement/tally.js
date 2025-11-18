5ea87b74a5aaa478a1e150f075dbc83edbd31cf3
Pirikita <beatriz.oliveira.work@gmail.com>
2025-11-18 11:35:38 +0100

    code for duplicate items node test to check

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -5,8 +5,22 @@ function tally(arr) {
     if (arr.length === 0) {
         return [];
     }
+
+    const result = {};
+
+    for (const item of arr) {
+        if (result[item] === undefined) {
+            result [item] = 1;
+        } else {
+            result[item] = result[item] + 1;
+        }
+    }
+    return result;
 }
+
+console.log(tally("dasda"));
 console.log(tally([]));
+console.log(tally(["a", "b", "c", "b", "a", "a"]));
 
 // Acceptance criteria:
 
