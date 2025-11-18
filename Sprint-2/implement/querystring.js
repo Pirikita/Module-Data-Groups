e4f7ebdfcaae3e6e65192132b70905b5012d4ca9
Pirikita <beatriz.oliveira.work@gmail.com>
2025-11-18 10:55:18 +0100

    - Fixed the code with the help of reading materials and Ai - node test

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,16 +1,31 @@
 function parseQueryString(queryString) {
   const queryParams = {};
-  if (queryString.length === 0) {
-    return queryParams;
+
+  if (!queryString) return queryParams;
+// remove leading "?"
+  if (queryString.startsWith("?")) {
+    queryString = queryString.slice(1);
   }
+  
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    if (!pair) continue;
+
+    const [key, ...rest] = pair.split("=");
+    const value = rest.join("=") // handles  "=" inside values
+
+    const decodedKey = decodeURIComponent(key); // used to make the URL readable. e.g.: from { name: "John%20Doe" } to { name: "John Doe" } 
+    const decodedValue = value ? decodeURIComponent(value) : "";
+
+    queryParams[decodedKey] = decodedValue;
   }
 
   return queryParams;
 }
+
+console.log(parseQueryString("name=oliveira&age=28"));
+console.log(parseQueryString("name=Beatriz%20Oliveira&city=Brussels"));
+
 
 module.exports = parseQueryString;
