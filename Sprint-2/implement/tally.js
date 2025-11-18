0c5b96ac4b14eeaef544774661d78b7cd4cdc70a
Pirikita <beatriz.oliveira.work@gmail.com>
2025-11-18 11:21:13 +0100

    Code to check if it's an array Code to deal with empty arrays

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,25 @@
-function tally() {}
+function tally(arr) {
+    if (!Array.isArray(arr)) {
+        return "THIS IS NOT AN ARRAY!";
+    }
+    if (arr.length === 0) {
+        return [];
+    }
+}
+console.log(tally([]));
+
+// Acceptance criteria:
+
+// Given a function called tally
+// When passed an array of items
+// Then it should return an object containing the count for each unique item
+
+
+
+// Given an array with duplicate items
+// When passed to tally
+// Then it should return counts for each unique item
+
+
 
 module.exports = tally;
