8b4e48fc21ca97ddef8baa836039f2710a32bcd4
Pirikita <beatriz.oliveira.work@gmail.com>
2025-11-18 14:07:05 +0100

    update the return of an empty array from [] to {}

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -3,9 +3,11 @@ function tally(arr) {
         return "THIS IS NOT AN ARRAY!";
     }
     if (arr.length === 0) {
-        return [];
+        return {};
     }
-
+ /* ANOTHER WAY OF DOING THE DUPLICATE ITEMS
+result[item] = (result[item] || 0) + 1;
+ */
     const result = {};
 
     for (const item of arr) {
@@ -21,19 +23,6 @@ function tally(arr) {
 console.log(tally("dasda"));
 console.log(tally([]));
 console.log(tally(["a", "b", "c", "b", "a", "a"]));
-
-// Acceptance criteria:
-
-// Given a function called tally
-// When passed an array of items
-// Then it should return an object containing the count for each unique item
-
-
-
-// Given an array with duplicate items
-// When passed to tally
-// Then it should return counts for each unique item
-
 
 
 module.exports = tally;
