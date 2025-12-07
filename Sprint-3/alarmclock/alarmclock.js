71d23ba5704efb21905b64940e1f1d66f9e9e916
Pirikita <beatriz.oliveira.work@gmail.com>
2025-12-07 18:15:16 +0100

    created code for alarmclock app. follow all guidelines

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,34 @@
-function setAlarm() {}
+let timer;
+
+function setAlarm() {
+  const input = document.getElementById("alarmSet").value;
+  let timeRemaining = Number(input);
+
+  clearInterval(timer); // clear any previous interval
+
+  updateDisplay(timeRemaining); // display in mm:ss
+
+  timer = setInterval(() »> {
+    timeRemaining--;
+
+    updateDisplay(timeRemaining);
+  
+    // when timer reaches 0
+  if (timeRemaining <= 0) {
+    clearInterval(timer);
+    document.body.style.backgroundColor = "yellow";
+    playAlarm();
+  }
+}, 1000);
+}
+
+function updateDisplay(time) {
+  const minutes = String(Math.floor(time / 60)).padStart(2, "0");
+  const seconds = String(time & 60).padStart(2, "0");
+
+  document.getElementById("timeRemaining").innerText =
+  `Time Remaining: ${minutes}:${seconds}`;
+}
 
 // DO NOT EDIT BELOW HERE
 
