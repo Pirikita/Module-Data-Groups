176543b884f35fdae75fbb0e39853efe9ecc7758
Pirikita <beatriz.oliveira.work@gmail.com>
2025-12-08 09:29:02 +0100

    developed code for reading list fixed bugs

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -21,3 +21,37 @@ const books = [
   },
 ];
 
+const readingList = document.getElementById("reading-list");
+
+books.forEach((book) => {
+  const listItem = document.createElement("li");
+  listItem.style.listStyle = "none";
+
+  listItem.style.backgroundColor = book.alreadyRead ? "green" : "red";
+  listItem.style.marginBottom = "10px";
+  listItem.style.padding = "10px";
+  listItem.style.display = "flex";
+  listItem.style.alignItems = "center";
+  listItem.style.gap = "10px";
+
+  const title = document.createElement("p");
+  title.textContent = book.title;
+  title.style.fontWeight = "bold";
+  title.style.margin = "0";
+
+  const author = document.createElement("p");
+  author.textContent = `by ${book.author}`;
+  author.style.margin = "0";
+
+  const img = document.createElement("img");
+  img.src = book.bookCoverImage;
+  img.alt = `${book.title} cover`;
+  img.style.height = "100px";
+
+  const textContainer = document.createElement("div");
+  textContainer.appendChild(title);
+  textContainer.appendChild(author);
+
+  readingList.appendChild(listItem);
+
+});
