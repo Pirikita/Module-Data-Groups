1f0d8c7a3c30aad7d5d4b6bb51c702f9a5e970b1
Pirikita <beatriz.oliveira.work@gmail.com>
2025-12-12 16:14:15 +0100

    auto forward function

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -10,9 +10,14 @@ const images = [
 const imgElement = document.getElementById("carousel-img");
 const forwardBtn = document.getElementById("forward-btn");
 const backwardBtn = document.getElementById("backward-btn");
+const autoForwardBtn = document.getElementById("auto-forward-btn");
+const autoBackwardBtn = document.getElementById("auto-backward-btn");
+const stopBtn = document.getElementById("stop-btn");
 
 // keep track of which image it's showing
 let currentIndex = 0;
+//store interval ID for auto slideshow
+let intervalID = null;
 
 // function to update image
 function updateImage() {
@@ -35,3 +40,13 @@ backwardBtn.addEventListener("click", function () {
         currentIndex = images.lenght -1; // go to last image
     }
 })
+
+// auto forward
+autoForwardBtn.addEventListener("click", function () {
+    clearInterval(intervalId);
+    intervalID = setInterval(function () {
+        currentIndex = (currentIndex + 1) % images.length;
+        updateImage();
+    }, 2000);// change at 2 sec
+});
+
